3669974d6f144b789b28eba4d183c83dec17935d
Munish Satia <munish.satia@gmail.com>
2017-10-20 21:41:09 -0400

    Api Authentication App with identityserver 4

---
+++ openidconnectclient/Program.cs
@@ -0,0 +1,52 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel;
+using Newtonsoft.Json.Linq;
+
+namespace openidconnectclient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+       
+        private static async Task MainAsync()
+        {
+            var disco =  await DiscoveryClient.GetAsync("http://localhost:5000");
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "msatia", "msatia");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("demoapi");
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response =  await client.GetAsync("http://localhost:5001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content =  await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            
+
+        }
+
+       
+    }
+}
---
+++ openidprotectedapi/Controllers/TokenController.cs
@@ -0,0 +1,23 @@
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace openidapiclient.Controllers
+{
+    public class TokenController : Controller
+    {
+        public async Task<IActionResult> GetTokenAsync(string clientid,string clientsecret)
+        {
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+            var tokenClient = new TokenClient(disco.TokenEndpoint,clientid,clientsecret);
+            var resp = await tokenClient.RequestClientCredentialsAsync("api1");
+            if (resp.IsError)
+                return new JsonResult(resp.Error);
+            else
+                return new JsonResult(resp.Json);
+        }
+    }
+}
